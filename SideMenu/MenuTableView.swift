6aa7d754a1bdc507f3f974ede8131e28fd07d149
jperezg86 <jperezg86@gmail.com>
2015-12-17 17:09:17 -0600

    Se agrega tableHeaderView al menu

--- SideMenu/MenuTableView.swift
+++ SideMenu/MenuTableView.swift
@@ -13,6 +13,9 @@ class MenuTableView: UITableViewController {
     
     override func viewDidLoad() {
         menuItems = ["Opcion 1","Opcion 2","Opcion 3"]
+        let headerView  = MenuHeaderView(frame: CGRect(x: 0.0, y: 0.0, width: 200.0, height: 200.0))
+        self.tableView.tableHeaderView = headerView
+
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
